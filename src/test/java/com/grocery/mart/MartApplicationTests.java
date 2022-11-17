9b6d117950f2643138dfa88f57255884be0e8d02
Rustem <scorus0808@gmail.com>
2022-11-16 22:04:38 -0500

    Added Spring project

---
+++ src/test/java/com/grocery/mart/MartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
